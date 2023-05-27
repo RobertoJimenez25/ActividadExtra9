5e1d2270b9de1960fa819a7a12ca9dd68c5a5624
RobertoJimenez25 <125772135+RobertoJimenez25@users.noreply.github.com>
2023-05-27 14:52:51 -0600

    Add files via upload

---
+++ ActividadExtra9/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.controlador.ControladorLibro;
+import org.example.modelo.Libro;
+import org.example.vista.VentanaLibro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        VentanaLibro view = new VentanaLibro("MVC y JDBC");
+        ControladorLibro controller = new ControladorLibro(view);
+
+    }
+}
